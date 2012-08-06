8d54ab52347ca9bdc5c922eebe0a0ca82d255e74
James Allardice <jim.allardice@gmail.com>
2012-08-06 20:31:39 +0100

    Added feature detect

--- Patterns.js
+++ Patterns.js
@@ -11,4 +11,23 @@ var Placeholders = (function () {
 		"email"
 	];
 
+	function init(opts) {
+
+		//Create an input element to test for the presence of the pattern property. If the pattern property exists, stop.
+		var test = document.createElement("input");
+
+		//Test input element for presence of pattern property. If it doesn't exist, the browser does not support the HTML5 pattern attribute
+		if (!test.pattern) {
+
+		}
+
+		//Pattern attribute already supported by browser :)
+		return false;
+	}
+
+	//Expose public methods
+	return {
+		init: init
+	};
+
 }());
