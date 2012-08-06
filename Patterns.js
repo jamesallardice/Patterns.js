2c8b2ae7a1e0bffd1789da6e66527be167fca0b0
James Allardice <jim.allardice@gmail.com>
2012-08-06 20:19:32 +0100

    Initial template for polyfill

---
+++ Patterns.js
@@ -0,0 +1,14 @@
+var Placeholders = (function () {
+
+	"use strict";
+
+	// These are the only input types that support the pattern attribute. See https://developer.mozilla.org/en-US/docs/HTML/Element/Input#Attributes
+	var validTypes = [
+		"text",
+		"search",
+		"tel",
+		"url",
+		"email"
+	];
+
+}());
