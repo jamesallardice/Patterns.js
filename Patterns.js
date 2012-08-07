6d35573825282ec48768b06693960ab113c73466
James Allardice <jim.allardice@gmail.com>
2012-08-07 08:40:41 +0100

    Display a tooltip under the first input that fails to match its pattern. Currently no styles applied.

--- Patterns.js
+++ Patterns.js
@@ -4,12 +4,51 @@ var Patterns = (function () {
 
 	// These are the only input types that support the pattern attribute. See https://developer.mozilla.org/en-US/docs/HTML/Element/Input#Attributes
 	var validTypes = [
-		"text",
-		"search",
-		"tel",
-		"url",
-		"email"
-	];
+			"text",
+			"search",
+			"tel",
+			"url",
+			"email"
+		],
+		defaultMessage,
+		tooltip;
+
+	//The getTooltipPosition function returns the position a tooltip should be placed in, relative to the document, depending on the input it applies to
+	function getTooltipPosition(elem) {
+
+		var boundingClientRect;
+
+		if (elem.getBoundingClientRect) {
+
+			//Get the position of the element using the native method as defined in the CSSOM spec
+			boundingClientRect = elem.getBoundingClientRect();
+			
+			//Only return the properties we need (for consistency with polyfill below), adjusting the top so the tooltip appears under the input
+			return {
+				left: boundingClientRect.left,
+				top: boundingClientRect.top + elem.offsetHeight
+			};
+
+		} else {
+
+			//The native getBoundingClientRect method isn't supported, so semi-polyfill it (only the bits we really need)
+			var x = 0,
+				y = 0;
+
+			//Loop through the elements offset parents, maintaining a running offset total
+			do {
+				x += elem.offsetLeft - elem.scrollLeft;
+				y += elem.offsetTop - elem.scrollTop;
+			} 
+			while (elem = elem.offsetParent);
+
+			//Return the offset values, adjusting the top so the tooltip appears under the input
+			return { 
+				left: x,
+				top: y + elem.offsetHeight
+			};
+		}
+	}
 
 	/* The submitHandler function is executed when the containing form, if any, of a given input element is submitted. If necessary, patterns on any
 	 * input element descendants of the form are executed and the form submission is prevented if any fail to match the value of that input */
@@ -19,6 +58,7 @@ var Patterns = (function () {
 			element,
 			pattern,
 			rPattern,
+			tooltipOffset,
 			i;
 
 		//Iterate over all descendant input elements and attempt to match pattern with value
@@ -42,7 +82,18 @@ var Patterns = (function () {
 				//Test the value of the input element against the pattern
 				if (!rPattern.test(element.value)) {
 
-					//The pattern didn't match, so prevent the form from being submitted
+					//The pattern didn't match. Get the position of the input so we can display a tooltip
+					tooltipOffset = getTooltipPosition(element);
+
+					//Set the content of the tooltip to the value of the title attribute (as per spec)
+					tooltip.innerHTML = element.getAttribute("title") || defaultMessage;
+
+					//Set the position of the tooltip and display it
+					tooltip.style.left = tooltipOffset.left;
+					tooltip.style.top = tooltipOffset.top;
+					tooltip.style.display = "block";
+
+					//Prevent the form from being submitted
 					return false;
 				}
 
@@ -62,7 +113,8 @@ var Patterns = (function () {
 			i,
 			form;
 
-		//Iterate over all input elements and apply placeholder polyfill if necessary
+		/* Iterate over all input elements and apply placeholder polyfill if necessary. We can't delegate the event higher up the tree 
+		 * because the submit event doesn't bubble in IE < 8 */
 		for (i = 0; i < numForms; i++) {
 
 			form = forms[i];
@@ -99,6 +151,17 @@ var Patterns = (function () {
 					return -1;
 				};
 			}
+
+			//This is the default message currently displayed by all browsers when no title attribute is present on the input element
+			defaultMessage = "Please match the requested format.";
+
+			//Create the element that will be used as the tooltip (displayed when an input value doesn't match its pattern)
+			tooltip = document.createElement("div");
+			tooltip.style.position = "absolute";
+			tooltip.style.display = "none";
+
+			//Append the tooltip element to the DOM (it's hidden at this point)
+			document.body.appendChild(tooltip);
 
 			setupPatterns();
 
